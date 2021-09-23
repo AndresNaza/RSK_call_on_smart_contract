caf7e0a3e3ecbb0cb5595ba7aef16e3c5f6227fc
Andres <andres.nazarian@hotmail.com>
2021-09-23 13:39:17 -0300

    Updates

---
+++ precache-manifest.df2e6f8b2bd54c86d0790121e2ecef32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f92d445e272c22606c50a376a7227a4",
+    "url": "/RSK_call_on_smart_contract/index.html"
+  },
+  {
+    "revision": "63ce75ba269eba864797",
+    "url": "/RSK_call_on_smart_contract/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bb5f935cfe4dce478147",
+    "url": "/RSK_call_on_smart_contract/static/js/2.c29ed938.chunk.js"
+  },
+  {
+    "revision": "63ce75ba269eba864797",
+    "url": "/RSK_call_on_smart_contract/static/js/main.a8ac3875.chunk.js"
+  },
+  {
+    "revision": "756659d032d338174774",
+    "url": "/RSK_call_on_smart_contract/static/js/runtime-main.e7c8c7e4.js"
+  }
+]);
